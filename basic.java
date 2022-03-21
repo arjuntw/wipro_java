ecfec3adc3094607717e036b628c810d5d14189e
arjun <arjuntw27@gmail.com>
2022-03-21 16:49:05 +0530

    my first java prg

---
+++ basic.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class basic {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        Scanner sc= new Scanner(System.in);
+        System.out.println("enter the first data");
+        int a= sc.nextInt();
+        System.out.println("enter a char");
+        String b= sc.next();
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
